699c7a1e87b8d2981cefa2c33027f4cd14bd3be4
JeffNaef <jeffrey.naef@stat.math.ethz.ch>
2020-03-27 15:17:28 +0100

    Update test-extraTorus.R

--- torus/tests/test-extraTorus.R
+++ torus/tests/test-extraTorus.R
@@ -41,4 +41,62 @@ plot(rbind(X.NA.grid,X.NA), cex = weights[2,]*500,pch=19)
 
 spatstat::weighted.quantile(x = rbind(X.NA.grid,X.NA)[,2], w = weights[1,], probs = c(0.05,0.95))
 spatstat::weighted.quantile(x = rbind(X.NA.grid,X.NA)[,2], w = weights[2,], probs = c(0.05,0.95))
+            
+            
+            
+
+
+######## nonlinear examples
+
+
+n <- 1000 # number of observations
+d <- 2 # number of dimensions
+
+X1 <- rnorm(n)
+X2<- -2*X1^2 + rnorm(n)*0.5
+
+X=cbind(X1,X2)
+
+
+
+# NA filter
+na <- apply(X,2,function(x) sample(c(0,1), size = length(x), prob = c(0.95, 0.05), replace = TRUE))
+X.NA <- X
+X.NA[na==1] <- NA
+X.NA.grid <- matrix(c(-2, NA, 2, NA, NA, 0),ncol=2,byrow = T)
+
+# plot the data
+plot(X,pch=19)
+
+# generate an extraTorus
+tor1 <- extraTorus(X = X.NA, nb.nodes = 4)
+tor2 <- extraTorus(X = X.NA, nb.nodes = 4)
+tor3 <- extraTorus(X = X.NA, nb.nodes = 4)
+tor4 <- extraTorus(X = X.NA, nb.nodes = 4)
+tor5 <- extraTorus(X = X.NA, nb.nodes = 4)
+tor6 <- extraTorus(X = X.NA, nb.nodes = 4)
+tor7 <- extraTorus(X = X.NA, nb.nodes = 4)
+tor8 <- extraTorus(X = X.NA, nb.nodes = 4)
+
+to <- combine(tor1, tor2, tor3, tor4, tor5, tor6, tor7, tor8)
+# get weights from stationary distr
+weights <- getSampleWeights(to, X = rbind(X.NA))
+
+# vizu results
+X.NAtrue<-X[!complete.cases(X.NA),]
+
+par(mfrow=c(1,2))
+plot(X.NA ,pch=19)
+points(X.NAtrue,pch=19, col="blue")
+
+# investigate imputed points
+X.indexed<-cbind(1:n, X.NA)
+X.imputed<- apply(X.indexed[!complete.cases(X.indexed),],1, function(x){ colSums(weights[x[1],]*X.NA, na.rm = T) } )
+X.imputed <- t(X.imputed)
+
+
+plot(X.NA ,pch=19)
+points(X.imputed,pch=19, col="red")
+
+
 
