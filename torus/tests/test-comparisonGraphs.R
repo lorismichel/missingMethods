ab0f9091b9f067430bc3887e768075e60452f73c
spohnm <metalina.spohn@stat.math.ethz.ch>
2020-04-02 13:08:27 +0200

    local changes ML

--- torus/tests/test-comparisonGraphs.R
+++ torus/tests/test-comparisonGraphs.R
@@ -10,10 +10,10 @@ plot(d$X,pch=19)
 
 # grid of interesting points
 X.NA.grid <- matrix(c(NA, -1,
-                      NA, -20, 
-                      NA, -12, 
-                      NA, -8, 
-                      NA, -4, 
+                      NA, -20,
+                      NA, -12,
+                      NA, -8,
+                      NA, -4,
                       NA, 0),ncol=2,byrow = T)
 
 
@@ -38,11 +38,11 @@ st_ct_all <- stationaryDistr(ct, rbind(X.NA.grid,d$X.NA), prob = prob, method = 
 st_ct_leaves  <- stationaryDistr(ct, rbind(X.NA.grid,d$X.NA), prob = prob, subset = id.leaves, method = "eigen")
 
 
-par(mfrow=c(5,3))
+par(mfrow=c(3,2))
 ## looking at the points
 for (i in 1:nrow(X.NA.grid)) {
   plot(st_et[i,],pch=19)
-  plot(st_ct_all[i,],pch=19)
+  #plot(st_ct_all[i,],pch=19)
   plot(st_ct_leaves[i,],pch=19)
 }
 
@@ -53,27 +53,27 @@ getTransitionMatrix(object = ct, x = c(NA,2))
 
 par(mfrow=c(3,1))
 
-ct <- closedTree(X = d$X.NA, depth = 6)
+ct <- closedTree(X = d$X.NA, depth = 7)
 et <- extraTorus(X = d$X.NA, nb.nodes = 4)
 
-prob <- c(0.0001,0.0001,1-2*0.0001)
-w_et <- getSampleWeights(et, X = rbind(X.NA.grid,d$X.NA), 
+prob <- c(0.01,0.01,1-2*0.01)
+w_et <- getSampleWeights(et, X = rbind(X.NA.grid,d$X.NA),
                             prob = prob, method = "eigen")
 w_ct_all <- getSampleWeights(ct, X = rbind(X.NA.grid,d$X.NA),
                              prob = prob, method = "eigen")
-w_ct_leaves <- getSampleWeights(ct, X = rbind(X.NA.grid,d$X.NA), 
+w_ct_leaves <- getSampleWeights(ct, X = rbind(X.NA.grid,d$X.NA),
                                 prob = prob,
                             subset = id.leaves, method = "eigen")
 
-par(mfrow=c(6,3))
+par(mfrow=c(6,1))
 ## looking at the points
 for (i in 1:nrow(X.NA.grid)) {
+  #plot(d$X.NA,pch=19)
+  #points(rbind(X.NA.grid,d$X.NA)[order(w_et[i,], decreasing = TRUE)[1:200],],col="red",pch=19)
+  #plot(d$X.NA,pch=19)
+  #points(rbind(X.NA.grid,d$X.NA)[order(w_ct_all[i,], decreasing = TRUE)[1:100],],col="red",pch=19)
   plot(d$X.NA,pch=19)
-  points(rbind(X.NA.grid,d$X.NA)[order(w_et[i,], decreasing = TRUE)[1:200],],col="red",pch=19)
-  plot(d$X.NA,pch=19)
-  points(rbind(X.NA.grid,d$X.NA)[order(w_ct_all[i,], decreasing = TRUE)[1:100],],col="red",pch=19)
-  plot(d$X.NA,pch=19)
-  points(rbind(X.NA.grid,d$X.NA)[order(w_ct_leaves[i,], decreasing = TRUE)[1:100],],col="red",pch=19)
+  points(rbind(X.NA.grid,d$X.NA)[order(w_ct_leaves[i,], decreasing = TRUE)[1:200],],col="red",pch=19)
 }
 
 
@@ -93,7 +93,7 @@ ct4 <- closedTree(X = X.NA, depth = 3)
 ct5 <- closedTree(X = X.NA, depth = 3)
 
 f <- combine(ct1,ct2,ct3,ct4,ct5)
-w_f <- getSampleWeightsEnsemble(f, X = rbind(X.NA.grid,X.NA), 
+w_f <- getSampleWeightsEnsemble(f, X = rbind(X.NA.grid,X.NA),
                          prob = c(0.005,0.005,1-2*0.005), subset = id.leaves)
 
 
