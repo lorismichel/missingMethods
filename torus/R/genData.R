c87344ae51f2b49773ff0a7d8380ba850d966649
spohnm <metalina.spohn@stat.math.ethz.ch>
2020-04-07 14:41:52 +0200

    updates to gendata ML

--- torus/R/genData.R
+++ torus/R/genData.R
@@ -1,14 +1,8 @@
-genData <- function(n, dataset = "bivariateGaussian", pNA = 0.05) {
+genData <- function(n,d=2, dataset = "bivariateGaussian", pNA = 0.05) {
 
   if (dataset == "bivariateGaussian") {
-    # data
-    n <- 500 # number of observations
-    d <- 2 # number of dimensions
-
     X <- MASS::mvrnorm(n = n, mu = rep(0, d),
                        Sigma = diag(0.3,d,d)+matrix(0.7,d,d)) # naive matrix example
-    Xtest <- MASS::mvrnorm(n = n, mu = rep(0, d),
-                           Sigma = diag(0.3,d,d)+matrix(0.7,d,d)) # naive matrix example
 
     # NA filter
     na <- apply(X,2,function(x) sample(c(0,1), size = length(x), prob = c(1-pNA, pNA), replace = TRUE))
@@ -19,16 +13,8 @@ genData <- function(n, dataset = "bivariateGaussian", pNA = 0.05) {
 
   } else {
 
-    d <- 2
-
-    #X1 <- rnorm(n)
-    #X2<- -2*X1^2 + rnorm(n)*0.5
-    #X <- cbind(X1,X2)
-
-
     x = runif(n,-1,1)
     X = cbind(x, -x^2+1 + rnorm(n)*0.02)
-
 
     # NA filter
     na <- apply(X,2,function(x) sample(c(0,1), size = length(x), prob = c(1-pNA, pNA), replace = TRUE))
@@ -37,8 +23,5 @@ genData <- function(n, dataset = "bivariateGaussian", pNA = 0.05) {
 
     return(list(X = X, X.NA = X.NA))
   }
-
-
-
 }
 
