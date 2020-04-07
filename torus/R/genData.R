e4ad4fea18d8e74876bc0d839a8344cd4fcb9fed
Jeff <jeffrey.naef@stat.math.ethz.ch>
2020-04-07 12:09:43 +0200

    Nicosdatageneration

--- torus/R/genData.R
+++ torus/R/genData.R
@@ -1,42 +1,44 @@
 genData <- function(n, dataset = "bivariateGaussian", pNA = 0.05) {
-  
+
   if (dataset == "bivariateGaussian") {
     # data
     n <- 500 # number of observations
     d <- 2 # number of dimensions
-    
+
     X <- MASS::mvrnorm(n = n, mu = rep(0, d),
                        Sigma = diag(0.3,d,d)+matrix(0.7,d,d)) # naive matrix example
     Xtest <- MASS::mvrnorm(n = n, mu = rep(0, d),
                            Sigma = diag(0.3,d,d)+matrix(0.7,d,d)) # naive matrix example
-    
+
     # NA filter
     na <- apply(X,2,function(x) sample(c(0,1), size = length(x), prob = c(1-pNA, pNA), replace = TRUE))
     X.NA <- X
     X.NA[na==1] <- NA
-    
+
     return(list(X = X, X.NA = X.NA))
-   
+
   } else {
-    
-    d <- 2 
-    
-    X1 <- rnorm(n)
-    X2<- -2*X1^2 + rnorm(n)*0.5
-    
-    X <- cbind(X1,X2)
-   
-    
-    
+
+    d <- 2
+
+    #X1 <- rnorm(n)
+    #X2<- -2*X1^2 + rnorm(n)*0.5
+    #X <- cbind(X1,X2)
+
+
+    x = runif(n,-1,1)
+    X = cbind(x, -x^2+1 + rnorm(n)*0.02)
+
+
     # NA filter
     na <- apply(X,2,function(x) sample(c(0,1), size = length(x), prob = c(1-pNA, pNA), replace = TRUE))
     X.NA <- X
     X.NA[na==1] <- NA
-    
+
     return(list(X = X, X.NA = X.NA))
   }
-  
-  
-  
+
+
+
 }
 
--- torus/tests/test-comparisonGraphs_2.R
+++ torus/tests/test-comparisonGraphs_2.R
@@ -3,7 +3,7 @@
 # use torus package
 require(torus)
 n<-500
-d <- genData(n =n , dataset = "parabola", pNA = 0)
+d <- genData(n =n , dataset = "bivariateGaussian", pNA = 0)
 
 # plot the data
 plot(d$X,pch=19)
@@ -19,7 +19,7 @@ X.NA.grid <- matrix(c(NA, -20,
 
 ct<-c()
 et<-c()
-B<-5
+B<-1
 
 if (B > 1) {
 
@@ -37,30 +37,46 @@ w_et <- getSampleWeightsEnsemble(et, X = rbind(X.NA.grid,d$X.NA),
                                      prob = c(0.005,0.005,1-2*0.005), d="k_divergence", method="eigen")
 }else{
 
-et <- extraTorus(X = d$X.NA, nb.nodes = 12)
+
+
+et <- extraTorus(X = d$X.NA, nb.nodes = 10)
 w_et<-getSampleWeights(et, X = rbind(X.NA.grid,d$X.NA),
-                                        prob = c(0.005,0.005,1-2*0.005), d="k_divergence", method="eigen")
+                                        prob = c(0.005,0.005,1-2*0.005), d="k_divergence", method="eigen" ) # ,  method = "power", power=2
+
+# around 2 min for both methods
+
+
+
+
 
 ct <- closedTree(X = d$X.NA, depth = 4)
 w_ct_all<-getSampleWeights(ct, X = rbind(X.NA.grid,d$X.NA),
-                       prob = c(0.005,0.005,1-2*0.005), d="euclidean", method="eigen")
+                       prob = c(0.005,0.005,1-2*0.005), d="k_divergence", method="eigen")
 
 }
 
 
 if (  sum(is.na(d$X.NA))==0 ){
 
+
   ## If we have no missing values, check nearest neigbours of the first point
-par(mfrow=c(1,1))
+par(mfrow=c(2,1))
 plot(d$X.NA)
 points(d$X.NA[order(w_et[6,7:(n+5)], decreasing = FALSE)[1:round(n/5)],], col="green")
 points(d$X.NA[1,1],d$X.NA[1,2], col="red" )
 
-par(mfrow=c(1,1))
+
 plot(d$X.NA)
-points(d$X.NA[order(w_ct_all[6,7:(n+5)], decreasing = FALSE)[1:round(n/10)],], col="green")
+points(d$X.NA[order(w_ct_all[6,7:(n+5)], decreasing = FALSE)[1:round(n/5)],], col="green")
 points(d$X.NA[1,1],d$X.NA[1,2], col="red" )
 }
+
+par(mfrow=c(1,1))
+plot(d$X.NA, cex=w_et[6,7:(n+5)]*100, pch=19)
+points(d$X.NA[1,1],d$X.NA[1,2], col="red" )
+
+
+
 
 
 par(mfrow=c(5,2))
